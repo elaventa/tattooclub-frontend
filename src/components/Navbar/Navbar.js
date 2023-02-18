699ee8d1dc257b7e6260406d1f52acbe0a7df1c6
Rahul T <rahulmanojcoder@gmail.com>
2023-02-18 16:01:19 +0530

    links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,29 +4,31 @@ import React, { useState } from "react"
 import "./Navbar.scss"
 
 const Navbar = () => {
-  const [clicked, setclicked] = useState(false);
+  const [clicked, setclicked] = useState(false)
   const handleClick = () => {
-      setclicked(!clicked);
-  };
+    setclicked(!clicked)
+  }
   return (
     <nav className={`navbar ${clicked}`}>
       <div className="logo">
-        <StaticImage
-          src="../../images/logo.png"
-          loading="eager"
-          quality={95}
-          formats={["auto", "webp", "avif"]}
-          alt=""
-          className="image"
-          style={{ width: "100%" }}
-        />
+        <Link to="/">
+          <StaticImage
+            src="../../images/logo.png"
+            loading="eager"
+            quality={95}
+            formats={["auto", "webp", "avif"]}
+            alt=""
+            className="image"
+            style={{ width: "100%" }}
+          />
+        </Link>
       </div>
 
       <ul className={`navItems ${clicked}`}>
-        <Link onClick={() => setclicked(false)} to="#">
+        <Link onClick={() => setclicked(false)} to="/portfolio">
           <li className="navItem">WORKS</li>
         </Link>
-        <Link onClick={() => setclicked(false)} to="#">
+        <Link onClick={() => setclicked(false)} to="/artists">
           <li className="navItem">ARTISTS</li>
         </Link>
         <Link onClick={() => setclicked(false)} to="#">

add699a926ca22691d06064790ba7799c4a5f17a
Rahul T <rahulmanojcoder@gmail.com>
2023-02-27 23:21:47 +0530

    bug fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import DiscoverTalent from '../components/DiscoverTalent/DiscoverTalent'
-import Hero from '../components/Hero/Hero'
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import SuccessStories from '../components/SuccessStories/SuccessStories'
